7059c1ea1bad8a26b0c7e23bae2d80861224d3d0
Pontakorn Prasertsuk <41262596+PeppaPigHS@users.noreply.github.com>
2019-02-07 09:59:48 +0700

    Too easy

---
+++ thailand-oi/o58_mar_c1_newroads.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define long long long
+#define iii tuple<int, int, int>
+
+using namespace std;
+
+const int N = 1e5+5;
+
+int n, m, weight[N<<1], cnt;
+int chk[N<<1], rev[N<<1];
+long sum, ret;
+vector<iii> g[N];
+
+void euler(int u) {
+    int v, idx, r;
+    while(!g[u].empty() && chk[get<1>(g[u].back())]) g[u].pop_back();
+    if(g[u].empty()) return;
+    iii now = g[u].back(); g[u].pop_back();
+    tie(v, idx, r) = now;
+    chk[idx] = 1, rev[idx] = r;
+    euler(v);
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for(int i = 1, a, b; i <= m; i++) {
+        scanf("%d %d %d", &a, &b, weight+i);
+        g[a].emplace_back(b, i, 0);
+        g[b].emplace_back(a, i, 1);
+        sum += weight[i];
+    }
+    euler(1);
+    for(int i = 1; i <= m; i++) if(rev[i]) ++cnt, ret += weight[i];
+    bool valid = (ret << 1) > sum ? 1 : 0;
+    printf("%d\n", valid ? m - cnt : cnt);
+    for(int i = 1; i <= m; i++) if(rev[i] ^ valid) printf("%d\n", i);
+
+    return 0; 
+}
