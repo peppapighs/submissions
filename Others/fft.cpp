d76402ee31e3ba6dc9c801d8970da5df53aebdfd
Pontakorn Prasertsuk <41262596+PeppaPigHS@users.noreply.github.com>
2019-02-13 15:54:51 +0700

    Too easy

--- Others/fft.cpp
+++ Others/fft.cpp
@@ -10,21 +10,18 @@ const int M = 1e9+7;
 const double PI = 4*atan(1);
 
 void fft(vector<base> &a) {
-    int n = a.size();
-    for(int i = 1, j = 0; i < n; i++) {
-        int k = n >> 1;
-        while(j >= k) j -= k, k >>= 1;
-        j += k;
-        if(i < j) swap(a[i], a[j]);
+    int n = a.size(), L = 0;
+    while((1 << L) < n) ++L;
+    for(int i = 0, pos = 0; i < n; i++, pos = 0) {
+        for(int j = 0; j < L; j++) pos |= (i & (1 << j)) << (L - j - 1);
+        if(i < pos) swap(a[i], a[pos]);
     }
     for(int len = 2; len <= n; len <<= 1) {
         double ang = 2 * PI / len;
         int half = len >> 1;
-        base omega(cos(ang), sin(ang)), w0(1);
-        vector<base> w(half);
-        for(int i = 0; i < half; i++) w[i] = w0, w0 *= omega;
-        for(int i = 0; i < n; i += len) for(int j = 0; j < half; j++) {
-            base u = a[i + j], v = a[i + j + half] * w[j];
+        base omega(cos(ang), sin(ang)), w(1);
+        for(int i = 0; i < n; i += len, w = base(1)) for(int j = 0; j < half; j++, w *= omega) {
+            base u = a[i + j], v = a[i + j + half] * w;
             a[i + j] = u + v;
             a[i + j + half] = u - v;
         }
@@ -35,12 +32,9 @@ vector<int> fft_mul(vector<int> a, vector<int> b) {
     int n = 1;
     vector<base> fa(all(a)), fb(all(b));
     while(a.size() + b.size() > n) n <<= 1;
-    fa.resize(n), fb.resize(n);
-
-    fft(fa), fft(fb);
+    fa.resize(n), fb.resize(n); fft(fa), fft(fb);
     for(int i = 0; i < n; i++) fa[i] = conj(fa[i] * fb[i]);
     fft(fa);
-
     vector<int> ret(n);
     for(int i = 0; i < n; i++) ret[i] = !!(int)(fa[i].real() / n + 0.5);
     while(!ret.empty() && !ret.back()) ret.pop_back();
