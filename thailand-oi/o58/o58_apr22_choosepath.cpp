fdf256409301bd5e95dfeaa02d093a608f971470
PeppaPigHS <ppeppapig.stuff@gmail.com>
2019-03-20 17:23:44 +0700

    Too easy

--- thailand-oi/o58/o58_apr22_choosepath.cpp
+++ thailand-oi/o58/o58_apr22_choosepath.cpp
@@ -9,52 +9,30 @@ using namespace std;
 
 const int N = 2e5+5;
 
-int n, A[N], d[N], hv[N], in[N], out[N], pos[N];
-long sum[2][2], ans;
+int n, d[N];
+long A[N], sum[2][2], ans;
 vector<pii> g[N];
 
-int proc(int u, int p) {
-    static int idx = 0;
-    int sz = 1; pii ret(0, -1);
-    in[u] = ++idx, pos[idx] = u;
+void dfs(int u, int p) {
+    sum[d[u] & 1][A[u] > 0] += A[u];
     for(pii v : g[u]) if(v.x != p) {
         d[v.x] = d[u] + v.y;
-        int z = proc(v.x, u);
-        sz += z;
-        ret = max(ret, pii(z, v.x));
+        dfs(v.x, u);
     }
-    out[u] = idx, hv[u] = ret.y;
-    return sz;
-}
-
-void calc(int u) {
-    ans += sum[d[u] & 1][A[u] > 0] * A[u];
-    ans -= sum[~d[u] & 1][A[u] < 0] * A[u];
-}
-
-void add(int u, bool b) {
-    sum[d[u] & 1][A[u] > 0] += b ? -A[u] : A[u];
-}
-
-void dfs(int u, int p, bool keep) {
-    for(pii v : g[u]) if(v.x != p && v.x != hv[u]) dfs(v.x, u, 0);
-    if(hv[u] != -1) dfs(hv[u], u, 1);
-    calc(u), add(u, 0);
-    for(pii v : g[u]) if(v.x != p && v.x != hv[u]) {
-        for(int i = in[v.x]; i <= out[v.x]; i++) calc(pos[i]);
-        for(int i = in[v.x]; i <= out[v.x]; i++) add(pos[i], 0);
-    }
-    if(!keep) for(int i = in[u]; i <= out[u]; i++) add(pos[i], 1);
 }
 
 int main() {
-    scanf("%d %d", &n, A);
+    scanf("%d %lld", &n, A);
     for(int i = 1, a, b; i < n; i++) {
-        scanf("%d %d %d", A+i, &a, &b);
+        scanf("%lld %d %d", A+i, &a, &b);
         g[i].emplace_back(a, b), g[a].emplace_back(i, b);
     }
-    proc(0, 0), dfs(0, 0, 1);
-    printf("%lld\n", ans);
+    dfs(0, 0);
+    for(int i = 0; i < n; i++) {
+        ans += (sum[d[i] & 1][A[i] > 0] - A[i]) * A[i];
+        ans -= (sum[~d[i] & 1][A[i] < 0]) * A[i];
+    }
+    printf("%lld\n", ans / 2ll);
 
     return 0;
 }
