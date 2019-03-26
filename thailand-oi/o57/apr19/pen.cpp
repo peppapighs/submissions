879d811c0023a5dd0f3359947002088aaa3fa7bd
peppapighs <ppeppapig.stuff@gmail.com>
2019-03-26 15:01:43 +0700

    Too easy

---
+++ thailand-oi/o57/apr19/pen.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define iii tuple<int, int, int>
+
+using namespace std;
+
+struct item {
+    int x, id, mx, prio;
+    item *l, *r;
+    item(int x, int id) : x(x), id(id), mx(id), prio(rand()), l(NULL), r(NULL) { }
+};
+
+typedef item* pitem;
+
+int mx(pitem t) { return t ? t->mx : 0; }
+void updt(pitem t) { if(t) t->mx = max({mx(t->l), t->id, mx(t->r)}); }
+
+void split(pitem t, pitem &l, pitem &r, int key) {
+    if(!t) return void(l = r = NULL);
+    if(t->x <= key) split(t->r, t->r, r, key), l = t;
+    else split(t->l, l, t->l, key), r = t;
+    updt(t);
+}
+
+void merge(pitem &t, pitem l, pitem r) {
+    if(!l || !r) return void(t = l ? l : r);
+    if(l->prio < r->prio) merge(l->r, l->r, r), t = l;
+    else merge(r->l, l, r->l), t = r;
+    updt(t);
+}
+
+const int N = 1e5+5;
+
+int n, dp[N], pos[N];
+int x1[N], x2[N], y[N];
+vector<int> coord;
+pitem t[N];
+
+int get(int x) { return lower_bound(coord.begin(), coord.end(), x) - coord.begin() + 1; }
+
+void update(int x, int a, int b) {
+    for(int i = x; i <= (int)coord.size(); i += i & -i) {
+        pitem r;
+        split(t[i], t[i], r, a);
+        merge(t[i], t[i], new item(a, b));
+        merge(t[i], t[i], r);
+    }
+}
+
+int query(int x, int r) {
+    int ret = 0;
+    if(!x) return 0;
+    for(int i = x; i; i -= i & -i) {
+        pitem l;
+        split(t[i], l, t[i], r);
+        ret = max(ret, mx(t[i]));
+        merge(t[i], l, t[i]);
+    }
+    return ret;
+}
+
+int main() {
+    iota(pos, pos+N, 0);
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        scanf("%d %d %d", x1+i, x2+i, y+i);
+        coord.emplace_back(x1[i]);
+    }
+    sort(pos+1, pos+n+1, [&](const int &a, const int &b) {
+        return y[a] < y[b];
+    });
+    sort(coord.begin(), coord.end());
+    coord.resize(unique(coord.begin(), coord.end()) - coord.begin());
+    for(int i = 1; i <= n; i++) {
+        int now = query(get(x2[pos[i]])-1, x2[pos[i]]);
+        dp[pos[i]] = dp[pos[now]] + 1;
+        update(get(x1[pos[i]]), x2[pos[i]], i);
+    }
+    for(int i = 1; i <= n; i++) printf("%d\n", dp[i]);
+
+    return 0;
+}
---
+++ thailand-oi/o58/apr23/desert.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define pii pair<int, int>
+#define x first
+#define y second
+
+using namespace std;
+
+const int N = 305;
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+
+int R, C, m;
+int A[N][N], vis[N][N];
+
+int get(int a, int b, int c, int d) { return A[c][d] - A[a-1][d] - A[c][b-1] + A[a-1][b-1]; }
+
+bool chk(int h, int w) {
+    if(get(1, 1, h, w)) return false;
+    queue<pii> Q;
+    memset(vis, 0, sizeof vis);
+    Q.emplace(h, w), vis[h][w] = true;
+    while(!Q.empty()) {
+        pii now = Q.front(); Q.pop();
+        for(int i = 0; i < 4; i++) {
+            int nr = now.x + dx[i], nc = now.y + dy[i];
+            if(nr - h + 1 < 1 || nr > R || nc - w + 1 < 1 || nc > C) continue;
+            if(get(nr - h + 1, nc - w + 1, nr, nc)) continue;
+            if(!vis[nr][nc]) {
+                vis[nr][nc] = true;
+                Q.emplace(nr, nc);
+            }
+        }
+    }
+    return vis[R][C];
+}
+
+int main() {
+    scanf("%d %d %d", &R, &C, &m);
+    for(int i = 1, a, b; i <= m; i++) {
+        scanf("%d %d", &a, &b);
+        A[++a][++b] = 1;
+    }
+    for(int i = 1; i <= R; i++) for(int j = 1; j <= C; j++)
+        A[i][j] += A[i-1][j] + A[i][j-1] - A[i-1][j-1];
+    int ans = 0;
+    for(int i = 1; i <= R; i++) {
+        int l = 1, r = C;
+        bool valid = false;
+        while(l < r) {
+            int mid = (l + r + 1) >> 1;
+            if(chk(i, mid)) l = mid, valid = true;
+            else r = mid - 1;
+        }
+        if(valid) ans = max(ans, i * l);
+    }
+    printf("%d\n", ans);
+
+    return 0;
+}
