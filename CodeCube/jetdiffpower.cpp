dbd243f92ec8309cedfc68ecd4bcbcc4cf62c860
PeppaPigHS <ppeppapig.stuff@gmail.com>
2019-04-14 10:39:38 +0700

    Too easy

--- CodeCube/jetdiffpower.cpp
+++ CodeCube/jetdiffpower.cpp
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
 
+#define long long long
+
 using namespace std;
 
 const int N = 2e5+5;
 
-int n, A;
-int S[N];
-long long ret;
-
-long long sq(int a) { return 1LL*a*a; }
+int n, C;
+long A[N], dp[105], pre[105], suf[105];
 
 int main() {
 	scanf("%d", &n);
-	for(int i = 1; i <= n; i++) scanf("%d", &S[i]);
-	scanf("%d", &A);
-	if(n == 1) return !printf("%d\n", A*S[1]);
-	if(S[1] < S[2]) {
-		if(A/2 < S[2]-S[1]) S[1] += A/2, ret += sq(A/2);
-		else S[1] += S[2]-S[1], ret += sq(S[2]-S[1]);
+	for(int i = 1; i <= n; i++) scanf("%lld", A+i);
+	scanf("%d", &C);
+	for(int i = 1; i <= n; i++) {
+		for(int j = 0; j <= 100; j++) {
+			if(i == 1) dp[j] = j * j;
+			else {
+				int x = A[i] - A[i-1] + j;
+				dp[j] = 1e18;
+				if(x >= 0) dp[j] = min(dp[j], pre[min(x, 100)] + C * x);
+				if(x <= 100) dp[j] = min(dp[j], suf[max(x, 0)] - C * x);
+				dp[j] += j * j;
+			}
+		}
+		for(int j = 0; j <= 100; j++) {
+			pre[j] = dp[j] - C * j;
+			if(j) pre[j] = min(pre[j], pre[j-1]);
+		}
+		for(int j = 100; ~j; j--) {
+			suf[j] = dp[j] + C * j;
+			if(j != 100) suf[j] = min(suf[j], suf[j+1]);
+		}
 	}
-	if(S[n-1] < S[n]) {
-		if(A/2 < S[n]-S[n-1]) S[n-1] += A/2, ret += sq(A/2);
-		else S[n-1] += S[n]-S[n-1], ret += sq(S[n]-S[n-1]);
-	} 
-	for(int i = 2; i <= n-1; i++) if(S[i-1] > S[i] && S[i] < S[i+1]) {
-		int mn = min(S[i-1], S[i+1]);
-		if(A < mn-S[i]) S[i] += A, ret += sq(A);
-		else S[i] += mn-S[i], ret += sq(mn-S[i]);		
-	}
-	for(int i = 1; i < n; i++) ret += 1LL*A*(abs(S[i]-S[i+1]));
-	printf("%lld\n", ret);
+	long ans = 1e18;
+	for(int i = 0; i <= 100; i++) ans = min(ans, dp[i]);
+	printf("%lld\n", ans);
 
 	return 0;
 }
