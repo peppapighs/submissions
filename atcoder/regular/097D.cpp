56f2892833f0549608ab200e3439979965fee09b
PeppaPig <peppapig@localhost.localdomain>
2019-10-07 18:18:41 +0700

    Too easy

---
+++ atcoder/regular/097D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5+5;
+
+int n, m, ans;
+int pos[N], dsu[N];
+
+int find(int x) { return dsu[x] = x == dsu[x] ? x : find(dsu[x]); }
+
+int main() {
+    iota(dsu, dsu+N, 0);
+    scanf("%d %d", &n, &m);
+    for(int i = 1, a; i <= n; i++) {
+        scanf("%d", &a);
+        pos[a] = i;
+    }
+    for(int i = 1, a, b; i <= m; i++) {
+        scanf("%d %d", &a, &b);
+        dsu[find(a)] = find(b); 
+    }
+    for(int i = 1; i <= n; i++) if(find(pos[i]) == find(i))
+        ++ans;
+    printf("%d\n", ans);
+
+    return 0;
+}
