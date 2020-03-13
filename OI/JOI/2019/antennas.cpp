24de6a1680a980671604eef955880e410027dc15
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-03-13 14:54:51 +0700

    Too easy

--- OI/JOI/2019/antennas.cpp
+++ OI/JOI/2019/antennas.cpp
@@ -10,66 +10,62 @@ const int N = 1 << 18;
 const int INF = 1e9 + 1;
 
 int n, q;
+int t1[N << 1], t2[N << 1], lz[N << 1];
 
-int mn[N<<1], mx[N<<1], lo[N<<1], hi[N<<1], t[N<<1];
-
-void push(int p, int l, int r) {
-    t[p] = max({t[p], hi[p] - mn[p], mx[p] - lo[p]});
-    if(l != r) {
-        if(mn[p<<1] != INF) {
-            lo[p<<1] = min(lo[p<<1], lo[p]);
-            hi[p<<1] = max(hi[p<<1], hi[p]);
-        }
-        if(mn[p<<1|1] != INF) {
-            lo[p<<1|1] = min(lo[p<<1|1], lo[p]);
-            hi[p<<1|1] = max(hi[p<<1|1], hi[p]);
-        } 
-    }
-    lo[p] = INF, hi[p] = -INF;
+void tag(int p, int x) {
+    t2[p] = max(t2[p], t1[p] + x);
+    lz[p] = max(lz[p], x);
 }
 
-void tag(int x, int k, int p = 1, int l = 1, int r = n) {
+void push(int p, int l, int r) {
+    if(l != r) tag(p << 1, lz[p]), tag(p << 1 | 1, lz[p]);
+    lz[p] = -INF;
+}
+
+void update(int x, int k, int p = 1, int l = 1, int r = n) {
     push(p, l, r);
-    if(l == r) {
-        if(k == -1) mn[p] = INF, mx[p] = -INF;
-        else mn[p] = mx[p] = k;
-        return;
-    }
-    int mid = l + r >> 1;
-    if(x <= mid) tag(x, k, p<<1, l, mid);
-    else tag(x, k, p<<1|1, mid+1, r);
-    mn[p] = min(mn[p<<1], mn[p<<1|1]), mx[p] = max(mx[p<<1], mx[p<<1|1]);
+    if(l == r) return void(t1[p] = k);
+    int mid = (l + r) >> 1;
+    if(x <= mid) update(x, k, p<<1, l, mid);
+    else update(x, k, p<<1|1, mid+1, r);
+    t1[p] = max(t1[p<<1], t1[p<<1|1]), t2[p] = max(t2[p<<1], t2[p<<1|1]);
 }
 
 void add(int x, int y, int k, int p = 1, int l = 1, int r = n) {
+    if(x <= l && r <= y) return void(tag(p, k));
     push(p, l, r);
-    if(x > r || l > y || mn[p] == INF) return;
-    if(x <= l && r <= y) {
-        lo[p] = min(lo[p], k), hi[p] = max(hi[p], k);
-        push(p, l, r);
-        return;
-    }
-    int mid = l + r >> 1;
-    add(x, y, k, p<<1, l, mid), add(x, y, k, p<<1|1, mid+1, r);
-    mn[p] = min(mn[p<<1], mn[p<<1|1]), mx[p] = max(mx[p<<1], mx[p<<1|1]);
-    t[p] = max(t[p<<1], t[p<<1|1]);
+    int mid = (l + r) >> 1;
+    if(x <= mid) add(x, y, k, p<<1, l, mid);
+    if(mid < y) add(x, y, k, p<<1|1, mid+1, r);
+    t1[p] = max(t1[p<<1], t1[p<<1|1]), t2[p] = max(t2[p<<1], t2[p<<1|1]);
 }
 
 int query(int x, int y, int p = 1, int l = 1, int r = n) {
+    if(x <= l && r <= y) return t2[p];
     push(p, l, r);
-    if(x > r || l > y) return -INF;
-    if(x <= l && r <= y) return t[p];
-    int mid = l + r >> 1;
-    return max(query(x, y, p<<1, l, mid), query(x, y, p<<1|1, mid+1, r));
+    int mid = (l + r) >> 1, ret = -INF;
+    if(x <= mid) ret = max(ret, query(x, y, p<<1, l, mid));
+    if(mid < y) ret = max(ret, query(x, y, p<<1|1, mid+1, r));
+    return ret;
 }
 
 int H[N], A[N], B[N], ans[N];
 vector<int> in[N], out[N];
 vector<pii> Q[N];
 
+void solve() {
+    fill_n(t1, N << 1, -INF), fill_n(t2, N << 1, -INF), fill_n(lz, N << 1, -INF);
+    for(int i = 1; i <= n; i++) {
+        for(int x : in[i]) update(x, -H[x]);
+        if(i - A[i] > 0) add(max(i - B[i], 1), i - A[i], H[i]);
+        for(pii p : Q[i]) ans[p.y] = max(ans[p.y], query(p.x, i));
+        for(int x : out[i]) update(x, -INF);
+    }
+}
+
 int main() {
-    fill_n(mn, N<<1, INF), fill_n(mx, N<<1, -INF);
-    fill_n(lo, N<<1, INF), fill_n(hi, N<<1, -INF);
+    memset(ans, -1, sizeof ans);
+
     scanf("%d", &n);
     for(int i = 1; i <= n; i++) {
         scanf("%d %d %d", H+i, A+i, B+i);
@@ -81,16 +77,11 @@ int main() {
         scanf("%d %d", &a, &b);
         Q[b].emplace_back(a, i);
     }
-    for(int i = 1; i <= n; i++) {
-        for(int x : in[i]) tag(x, H[x]);
-        add(i - B[i], i - A[i], H[i]);
-        for(pii p : Q[i]) ans[p.y] = query(p.x, i);
-        for(int x : out[i]) tag(x, -1);
-    }
-    for(int i = 1; i <= q; i++) {
-        if(ans[i] > 0) printf("%d\n", ans[i]);
-        else printf("-1\n");
-    }
+    solve();
+    for(int i = 1; i <= n; i++) H[i] = -H[i];
+    solve();
+
+    for(int i = 1; i <= q; i++) printf("%d\n", ans[i]);
 
     return 0;
 }
