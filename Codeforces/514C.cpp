79b90c156b887005e0987b1d7bda89812b86982b
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-03-13 20:35:58 +0700

    Too easy

--- Codeforces/514C.cpp
+++ Codeforces/514C.cpp
@@ -1,37 +1,46 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 const int N = 6e5+5;
-const int P = 98765431;
+ 
+struct Trie {
+    int t[N][3], tag[N], ptr = 1;
+
+    void add(char *S, int p = 1, int i = 1) {
+        if(S[i] == '\0') return void(tag[p] = 1);
+        int &now = t[p][S[i] - 'a'];
+        if(!now) now = ++ptr;
+        add(S, now, i + 1);
+    }
+
+    bool find(char *S, int p = 1, int i = 1, int dif = 0) {
+        if(S[i] != '\0') {
+            int now = S[i] - 'a';
+            if(t[p][now] && find(S, t[p][now], i + 1, dif))
+                return true;
+            if(!dif)
+                for(int j = 0; j < 3; j++) if(j != now && t[p][j] && find(S, t[p][j], i + 1, 1))
+                    return true;
+        } else if(dif && tag[p]) return true;
+        return false;
+    }
+} trie;
 
 int n, m;
-long mpow[N];
 char S[N];
-set<long> st;
-
+ 
 int main() {
-    mpow[0] = 1;
-    for(int i = 1; i < N; i++) mpow[i] = mpow[i-1] * P;
-
     scanf("%d %d", &n, &m);
     for(int i = 1; i <= n; i++) {
-        long hsh = 0;
         scanf(" %s", S + 1);
-        for(int j = 1; S[j] != '\0'; j++) hsh += S[j] * mpow[j-1];
-        for(int j = 1; S[j] != '\0'; j++) {
-            hsh -= S[j] * mpow[j-1];
-            for(int k = 'a'; k <= 'c'; k++) st.emplace(hsh + k * mpow[j-1]);
-            hsh += S[j] * mpow[j-1];
-        }
-    }
+        trie.add(S);
+    } 
     for(int i = 1; i <= m; i++) {
-        long hsh = 0;
         scanf(" %s", S + 1);
-        for(int j = 1; S[j] != '\0'; j++) hsh += S[j] * mpow[j-1];
-        if(st.find(hsh) != st.end()) printf("YES\n");
+        if(trie.find(S)) printf("YES\n");
         else printf("NO\n");
     }
-
+ 
     return 0;
 }
