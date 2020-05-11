47a9bdd120f1cb014b25b05cd84b8b27b7aef148
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-05-11 16:41:20 +0700

    Too easy

--- thailand-oi/o61/may02/friendset.cpp
+++ thailand-oi/o61/may02/friendset.cpp
@@ -1,41 +1,35 @@
 #include <bits/stdc++.h>
 
+#define long long long
 #define pii pair<int, int>
 #define x first
 #define y second
 
 using namespace std;
 
-const int N = 1e5 + 5;
-
-pii add(pii a, pii b) { return pii(a.x + b.x, a.y + b.y); }
-pii sub(pii a, pii b) { return pii(a.x - b.x, a.y - b.y); }
-pii mul(pii a, pii b) { return pii(a.x * b.x, a.y * b.y); }
+const int N = 1e5+5;
+const int B = 1e9+7;
 
 int n, m;
-long long ans;
-vector<pii> edge;
-pii hsh[N];
-map<pii, int> cnt;
-
-pii prm(69, 420);
-pii pwr[N];
+long ans, ppow[N], hsh[N];
+vector<pii> E;
+map<long, int> mp;
 
 int main() {
-	pwr[0] = pii(1, 1);
-	for(int i = 1; i < N; i++) pwr[i] = mul(pwr[i - 1], prm);
-	scanf("%d%d", &n, &m);
-	for(int i = 0, u, v; i < m; i++) {
-		scanf("%d%d", &u, &v);
-		hsh[u] = add(hsh[u], pwr[v]);
-		hsh[v] = add(hsh[v], pwr[u]);
-		edge.emplace_back(u, v);
+	ppow[0] = 1;
+	for(int i = 1; i < N; i++) ppow[i] = ppow[i - 1] * B;
+
+	scanf("%d %d", &n, &m);
+	for(int i = 1, u, v; i <= m; i++) {
+		scanf("%d %d", &u, &v);
+		hsh[u] += ppow[v], hsh[v] += ppow[u];
+		E.emplace_back(u, v);
 	}
 
-	for(int i = 1; i <= n; i++) cnt[hsh[i]]++;
-	for(pair<pii, int> p : cnt) ans += (1LL * p.y * (p.y - 1LL) / 2LL);
-	for(pii p : edge) if(sub(hsh[p.x], pwr[p.y]) == sub(hsh[p.y], pwr[p.x])) ans++;
-
+	for(int i = 1; i <= n; i++) ++mp[hsh[i]];
+	for(auto p : mp) ans += 1ll * (p.y) * (p.y - 1) / 2;
+	for(pii e : E) if(hsh[e.x] - ppow[e.y] == hsh[e.y] - ppow[e.x])
+		++ans;
 	printf("%lld", ans);
 
 	return 0;
