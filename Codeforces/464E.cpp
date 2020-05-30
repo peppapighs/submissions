a15a3741405c571e84859879069f8742a570fa17
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-05-30 16:14:55 +0700

    Too easy

--- Codeforces/464E.cpp
+++ Codeforces/464E.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+#define long long long
 #define pii pair<int, int>
 #define x first
 #define y second
@@ -10,123 +11,128 @@ const int N = 1e5 + 100;
 const int M1 = 1e9 + 7;
 const int M2 = 1e9 + 9;
 
-pii add(pii a, pii b) { return {(a.x + b.x) % M1, (a.y + b.y) % M2}; }
-pii mul(pii a, pii b) { return {(a.x * b.x) % M1, (a.y * b.y) % M2}; }
+int ptr;
 
-pii power[N];
+struct Hash {
+    long h1, h2;
+    Hash() {}
+    Hash(long h) : h1(h % M1), h2(h % M2) {}
+    Hash(long h1, long h2) : h1(h1), h2(h2) {}
 
-struct item {
-	pii hsh;
-	item *l, *r;
-	item() {}
-	item(pii hsh, item *l, item *r) : hsh(hsh), l(l), r(r) {}
-};
+    bool empty() { return (h1 == 0 && h2 == 0); }
 
-int n, m, s, t;
-vector<vector<pii> > g(N);
-
-int par[N];
-item *dist[N];
-
-item *newleaf(pii a) {
-	return new item(a, nullptr, nullptr);
-}
-
-item *newpar(item *l, item *r) {
-	return new item(add(l->hsh, r->hsh), l, r);
-}
-
-item *build(bool inf, int l = 0, int r = N) {
-	if(l == r) {
-		if(inf) return newleaf(power[l]);
-		else return newleaf(pii(0, 0));
-	}
-	int mid = (l + r) >> 1;
-	return newpar(build(inf, l, mid), build(inf, mid + 1, r));
-}
-
-bool travel(int x, item *p, int l = 0, int r = N) {
-	if(l == r) return (p->hsh != pii(0, 0));
-	int mid = (l + r) >> 1;
-	if(x <= mid) return travel(x, p->l, l, mid);
-	else return travel(x, p->r, mid + 1, r);
-}
-
-item *update(int x, item *p, int l = 0, int r = N) {
-	if(l == r) {
-		if(p->hsh == pii(0, 0)) return newleaf(power[l]);
-		else return newleaf(pii(0, 0));
-	}
-	int mid = (l + r) >> 1;
-	if(x <= mid) return newpar(update(x, p->l, l, mid), p->r);
-	else return newpar(p->l, update(x, p->r, mid + 1, r));
-}
-
-bool compare(item *p1, item *p2, int l = 0, int r = N) {
-	if(l == r) {
-		if(p1->hsh == p2->hsh) return false;
-		else return p2->hsh != pii(0, 0);
-	}
-	int mid = (l + r) >> 1;
-	if(p1->r->hsh == p2->r->hsh) return compare(p1->l, p2->l, l, mid);
-	else return compare(p1->r, p2->r, mid + 1, r);
-}
-
-item *true_add(int x, item *p) {
-	item *ret = update(x, p);
-	while(!travel(x++, ret)) ret = update(x, ret);
-	return ret;
-}
+    friend Hash operator+(const Hash &a, const Hash &b) {
+        return Hash((a.h1 + b.h1) % M1, (a.h2 + b.h2) % M2);
+    }
+    friend Hash operator*(const Hash &a, const Hash &b) {
+        return Hash((a.h1 * b.h1) % M1, (a.h2 * b.h2) % M2);
+    }
+    friend bool operator==(const Hash &a, const Hash &b) {
+        return (a.h1 == b.h1 && a.h2 == b.h2);
+    }
+} mpow[N];
 
 struct node {
-	item *d;
-	int u;
-	node(item *d, int u) : d(d), u(u) {}
-	friend bool operator<(const node &a, const node &b) {
-		return !compare(a.d, b.d);
-	}
+    Hash h;
+    int l, r;
+    node() {}
+    node(Hash h, int l, int r) : h(h), l(l), r(r) {}
+} t[N * 100];
+
+int new_leaf(Hash hsh) {
+    t[++ptr] = node(hsh, -1, -1);
+    return ptr;
+}
+
+int new_par(int l, int r) {
+    t[++ptr] = node(t[l].h + t[r].h, l, r);
+    return ptr;
+}
+
+int build(bool inf, int l = 0, int r = N - 1) {
+    if(l == r) return new_leaf(inf ? mpow[l] : Hash(0));
+    int mid = (l + r) >> 1;
+    return new_par(build(inf, l, mid), build(inf, mid + 1, r));
+}
+
+bool get_pos(int x, int p, int l = 0, int r = N - 1) {
+    if(l == r) return (!t[p].h.empty()); 
+    int mid = (l + r) >> 1;
+    if(x <= mid) return get_pos(x, t[p].l, l, mid);
+    else return get_pos(x, t[p].r, mid + 1, r);
+}
+
+int increment(int x, int p, int l = 0, int r = N - 1) {
+    if(l == r) return new_leaf(t[p].h.empty() ? mpow[l] : Hash(0));
+    int mid = (l + r) >> 1;
+    if(x <= mid) return new_par(increment(x, t[p].l, l, mid), t[p].r);
+    else return new_par(t[p].l, increment(x, t[p].r, mid + 1, r));
+}
+
+int add(int x, int p) {
+    p = increment(x, p);
+    while(!get_pos(x++, p)) p = increment(x, p);
+    return p;
+}
+
+bool compare(int p1, int p2, int l = 0, int r = N - 1) {
+    if(l == r) return (t[p1].h.empty() && !t[p2].h.empty());
+    int mid = (l + r) >> 1;
+    if(t[t[p1].r].h == t[t[p2].r].h) return compare(t[p1].l, t[p2].l, l, mid);
+    else return compare(t[p1].r, t[p2].r, mid + 1, r);
+}
+
+struct item {
+    int d, u;
+    item(int d, int u) : d(d), u(u) {}
+    friend bool operator<(const item &a, const item &b) {
+        return !compare(a.d, b.d);
+    }
 };
 
-priority_queue<node> Q;
+int n, m, s, e;
+int d[N], par[N];
+vector<pii> g[N];
+priority_queue<item> Q;
 
 int main() {
-	power[0] = pii(1, 1);
-	for(int i = 1; i < N; i++) power[i] = mul(power[i - 1], pii(2, 2));
-	scanf("%d%d", &n, &m);
-	while(m--) {
-		int u, v, w;
-		scanf("%d%d%d", &u, &v, &w);
-		g[u].emplace_back(v, w);
-		g[v].emplace_back(u, w);
-	}
-	scanf("%d%d", &s, &t);
-	item *INF = build(true);
-	for(int i = 1; i <= n; i++) dist[i] = INF;
+    mpow[0] = Hash(1);
+    for(int i = 1; i < N; i++) mpow[i] = mpow[i - 1] * Hash(2);
+    
+    scanf("%d %d", &n, &m);
+    for(int i = 1, a, b, c; i <= m; i++) {
+        scanf("%d %d %d", &a, &b, &c);
+        g[a].emplace_back(b, c), g[b].emplace_back(a, c); 
+    }
+    scanf("%d %d", &s, &e);
 
-	dist[s] = build(false);
-	Q.emplace(dist[s], s);
+    int INF = build(true);
+    for(int i = 1; i <= n; i++) d[i] = INF;
 
-	while(!Q.empty()) {
-		node now = Q.top();
-		Q.pop();
-		if(dist[now.u] != now.d) continue;
-		for(pii v : g[now.u]) {
-			item *sum = true_add(v.y, dist[now.u]);
-			if(compare(sum, dist[v.x])) {
-				dist[v.x] = sum;
-				par[v.x] = now.u;
-				Q.emplace(dist[v.x], v.x);
-			}
-		}
-	}
+    par[s] = s;
+    Q.emplace(d[s] = build(false), s);
+    while(!Q.empty()) {
+        item u = Q.top(); Q.pop();
+        if(u.d != d[u.u]) continue;
+        for(pii v : g[u.u]) {
+            int sum = add(v.y, u.d);
+            if(compare(sum, d[v.x])) {
+                Q.emplace(d[v.x] = sum, v.x);
+                par[v.x] = u.u;
+            }
+        }
+    }
+    if(!par[e]) return !printf("-1\n");
 
-	if(dist[t] == INF) return !printf("-1");
-	vector<int> ans;
-	ans.emplace_back(t);
-	for(int i = par[t]; i; i = par[i]) ans.emplace_back(i);
-	printf("%d\n", dist[t]->hsh.x);
-	printf("%d\n", ans.size());
-	for(int i = ans.size() - 1; ~i; i--) printf("%d ", ans[i]);
+    printf("%lld\n", t[d[e]].h.h1);
+    vector<int> vec;
+    for(int i = e; vec.empty() || vec.back() != s; i = par[i])
+        vec.emplace_back(i);
+    reverse(vec.begin(), vec.end());
 
-	return 0;
+    printf("%d\n", (int)vec.size());
+    for(int u : vec) printf("%d ", u);
+    printf("\n");
+
+    return 0;
 }
