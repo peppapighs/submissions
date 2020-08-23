02467a39a3719125150ed72b17c2a8a22caf198b
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-23 19:18:18 +0700

    Too easy

---
+++ OI/BOI/2012/fire.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define long long long
+#define iii tuple<int, int, int>
+#define pii pair<int, int>
+#define x first
+#define y second
+
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int n, s;
+int x[N], y[N], id[N];
+vector<iii> event;
+
+long sum_x[8], sum_y[8], cnt[8];
+
+void add(int i, bool b) {
+    if(b) sum_x[id[i]] += x[i], sum_y[id[i]] += y[i], ++cnt[id[i]];
+    else sum_x[id[i]] -= x[i], sum_y[id[i]] -= y[i], --cnt[id[i]];
+}
+
+long get_answer(long fx) {
+    return sum_y[0]
+        + (sum_x[1] - fx * cnt[1])
+        + (sum_y[2] + (fx + s) * cnt[2] - sum_x[2])
+        + (sum_x[3] - fx * cnt[3] + cnt[3] * s - sum_y[3])
+        + (fx * cnt[4] - sum_x[4])
+        + (fx * cnt[5] - sum_x[5] + cnt[5] * s - sum_y[5])
+        + (sum_y[6] + sum_x[6] - (fx - s) * cnt[6])
+        + sum_y[7];
+}
+
+int main() {
+    scanf("%d %d", &n, &s);
+    for(int i = 1; i <= n; i++) {
+        scanf("%d %d", y + i, x + i), y[i] = abs(y[i]);
+        sum_x[0] += x[i], sum_y[0] += y[i], ++cnt[0];
+        if(y[i] < s) {
+            event.emplace_back(x[i] - s, 2, i);
+            event.emplace_back(x[i] - y[i], 3, i);
+            event.emplace_back(x[i], 5, i);
+            event.emplace_back(x[i] + y[i], 6, i);
+            event.emplace_back(x[i] + s, 7, i);
+        } else {
+            event.emplace_back(x[i] - y[i], 1, i);
+            event.emplace_back(x[i], 4, i);
+            event.emplace_back(x[i] + y[i], 7, i);
+        }
+    }
+    sort(event.begin(), event.end());
+
+    long ans = 1e18;
+    for(iii e : event) {
+        int fx, type, idx; tie(fx, type, idx) = e;
+        add(idx, 0);
+        id[idx] = type;
+        add(idx, 1);
+        ans = min(ans, get_answer(fx));
+    }
+    printf("%lld\n", ans);
+
+    return 0;
+}
---
+++ OI/BOI/2012/melody.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+#define pii pair<int, int>
+#define x first
+#define y second
+
+using namespace std;
+
+const int N = 105;
+const int M = 1e5 + 5;
+
+int n, m, s, g;
+char S[N][N];
+
+bool edge[N][N];
+
+void bfs(int src, int *dist, int *par) {
+    fill_n(dist, N, 1e9), memset(par, -1, sizeof par);
+
+    queue<pii> Q;
+    Q.emplace(src, dist[src] = 0), par[src] = 0;
+    while(!Q.empty()) {
+        pii u = Q.front(); Q.pop();
+        if(dist[u.x] != u.y) continue;
+        for(int i = 1; i <= n; i++) if(edge[u.x][i] && dist[i] == 1e9)
+            Q.emplace(i, dist[i] = u.y + 1), par[i] = u.x;
+    }
+}
+
+int dist[N][N], par[N][N];
+
+int A[M], dp[M], pre[M];
+vector<int> last[N];
+
+int main() {
+    fill_n(dp, M, 1e9), memset(pre, -1, sizeof pre);
+
+    scanf("%d %d %d", &n, &s, &g);
+    for(int i = 1; i <= n; i++) scanf(" %s", S[i] + 1), last[i].emplace_back(0);
+
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++) {
+        int cnt = 0;
+        for(int k = 1; k <= s; k++) cnt += (S[i][k] != S[j][k]);
+        if(cnt <= g) edge[i][j] = true;
+    }
+    for(int i = 1; i <= n; i++) bfs(i, dist[i], par[i]);
+
+    scanf("%d", &m);
+    for(int i = 1; i <= m; i++) {
+        scanf("%d", A + i);
+        last[A[i]].emplace_back(i);
+    }
+
+    dp[0] = 0;
+    for(int i = 1; i <= m; i++) for(int j = 1; j <= n; j++) {
+        auto it = upper_bound(last[j].begin(), last[j].end(), i - dist[A[i]][j] - (A[i] == j));
+        if(it == last[j].begin()) continue;
+
+        int idx = *--it;
+        if(dp[idx] + i - idx - 1 < dp[i]) {
+            dp[i] = dp[idx] + i - idx - 1;
+            pre[i] = idx;
+        }
+    }
+
+    int mn = 1e9, cur = -1;
+    vector<int> ans;
+    for(int i = 1; i <= m; i++) if(dp[i] + m - i < mn)
+        mn = dp[i] + m - i, cur = i;
+
+    printf("%d\n", mn);
+    for(int i = cur; i <= m; i++) ans.emplace_back(A[cur]);
+    while(cur) {
+        int nex = pre[cur];
+        if(!nex) for(int i = 1; i < cur; i++) ans.emplace_back(A[cur]);
+        else {
+            int sz = cur - nex, now = A[cur];
+            while(now != A[nex]) {
+                ans.emplace_back(par[A[nex]][now]);
+                now = par[A[nex]][now];
+                --sz;
+            }
+            for(int i = 1; i <= sz; i++) ans.emplace_back(A[nex]);
+        }
+        cur = nex;
+    }
+    assert((int)ans.size() == m);
+    reverse(ans.begin(), ans.end());
+
+    for(int x : ans) printf("%d ", x);
+    printf("\n");
+
+    return 0;
+}
