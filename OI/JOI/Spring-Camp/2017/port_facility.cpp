89e9a5e451064639cfa423a439f661a3c1e00a5f
PeppaPigHS <ptk.prasertsuk@gmail.com>
2022-03-20 17:12:35 +0800

    Too easy

--- OI/JOI/Spring-Camp/2017/port_facility.cpp
+++ OI/JOI/Spring-Camp/2017/port_facility.cpp
@@ -7,39 +7,98 @@
 
 using namespace std;
 
-const int N = 1e6+5;
-const int M = 1e9+7;
+const int N = 2e6 + 5;
+const int M = 1e9 + 7;
 
-int par[N << 1];
+class SegmentTree {
+private:
+    pii t[N << 1];
+public:
+    SegmentTree() {
+        fill_n(t, N << 1, pii(-M, -M));
+    }
 
-int find(int x) { return par[x] = x == par[x] ? x : find(par[x]); }
+    void update(int x, pii k) {
+        for(t[x += N] = k; x != 1; x >>= 1)
+            t[x >> 1] = max(t[x], t[x ^ 1]);
+    }
+
+    pii query(int l, int r) {
+        pii ret(-M, -M);
+        for(l += N, r += N + 1; l < r; l >>= 1, r >>= 1) {
+            if(l & 1) ret = max(ret, t[l++]);
+            if(r & 1) ret = max(ret, t[--r]);
+        }
+        return ret;
+    }
+} t_l, t_r;
 
 int n;
-vector<pii> vec;
-map<int, int> mp;
+int L[N], R[N], col[N];
+
+void dfs(int u, int c) {
+    col[u] = c;
+    for(int _ = L[u] + 1; _ <= R[u] - 1; _++) {
+        auto [r, v] = t_l.query(L[u] + 1, R[u] - 1);
+        if(r > R[u]) {
+            t_l.update(L[v], {-M, -M});
+            t_r.update(R[v], {-M, -M});
+            dfs(v, c ^ 1);
+        } else break;
+    }
+    for(int _ = L[u] + 1; _ <= R[u] - 1; _++) {
+        auto [l, v] = t_r.query(L[u] + 1, R[u] - 1);
+        if(-l < L[u]) {
+            t_l.update(L[v], {-M, -M});
+            t_r.update(R[v], {-M, -M});
+            dfs(v, c ^ 1);
+        } else break;
+    }
+}
 
 int main() {
-    iota(par, par + (N << 1), 0);
+    memset(col, -1, sizeof col);
 
     scanf("%d", &n);
-    for(int i = 1, x, y; i <= n; i++) {
-        scanf("%d %d", &x, &y);
-        vec.emplace_back(x, y);
-    }
-    sort(vec.begin(), vec.end());
     for(int i = 0; i < n; i++) {
-        auto st = mp.lower_bound(vec[i].x), en = mp.upper_bound(vec[i].y);
-        if(en != mp.begin()) for( ; st != en; st++) {
-            par[find(i)] = find(st->y + n);
-            par[find(i + n)] = find(st->y);
-            if(find(st->y) == find(prev(en)->y)) break;
-        }
-        mp[vec[i].y] = i;
+        scanf("%d %d", L + i, R + i);
+        --L[i], --R[i];
+        t_l.update(L[i], {R[i], i});
+        t_r.update(R[i], {-L[i], i});
     }
-    for(int i = 0; i < n; i++) if(find(i) == find(i + n)) return !printf("0\n");
+
     long ans = 1;
-    for(int i = 0; i < n; i++) if(find(i) == i) ans = ans * 2 % M;
-    printf("%lld\n", ans);
+    for(int i = 0; i < n; i++) if(col[i] == -1) {
+        ans = ans * 2 % M;
+        t_l.update(L[i], {-M, -M});
+        t_r.update(R[i], {-M, -M});
+        dfs(i, 0);
+    }
+
+    bool ok = true;
+    for(int color = 0; color <= 1; color++) {
+        for(int i = 0; i < n; i++) {
+            if(col[i] == color) {
+                t_l.update(L[i], {R[i], i});
+                t_r.update(R[i], {-L[i], i});
+            } else {
+                t_l.update(L[i], {-M, -M});
+                t_r.update(R[i], {-M, -M});
+            }
+        }
+        for(int i = 0; i < n; i++) if(col[i] == color) {
+            int r = t_l.query(L[i] + 1, R[i] - 1).x;
+            int l = -t_r.query(L[i] + 1, R[i] - 1).x;
+
+            if(r > R[i] || l < L[i]) {
+                ok = false;
+                break;
+            }
+        }
+    }
+
+    if(ok) printf("%lld\n", ans);
+    else printf("0\n");
 
     return 0;
 }
